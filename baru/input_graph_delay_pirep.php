bfbb7fa70873fcc5df92e36f655aa1136d06ce60
Kukuh Rilo Pambudi <rilokukuh@gmail.com>
2017-07-17 09:27:57 +0700

    membenarkan input graph dan mtbur ke frontend

--- baru/input_graph_delay_pirep.php
+++ baru/input_graph_delay_pirep.php
@@ -5,131 +5,116 @@
 	$res_actype = mysqli_query($link, $sql_actype);
 ?>
 
-<form action="graph.php" method="post" style="margin-bottom: 50px" id="form_graph">
-	<table>
-		<tbody>
-			<tr>
-				<th>
-					A/C Type
-				</th>
-				<th>
-					<select name="actype" style="">
-							<?php
-								$isSelect = "";
-								while($row = $res_actype->fetch_array(MYSQLI_NUM)){
-									if($row[0]==$_POST["actype"]){
-										$isSelect="selected";
-										echo "<option value=".$row[0]." ".$isSelect.">".$row[0]."</option>";
-									}
-									else{
-										echo "<option value=".$row[0].">".$row[0]."</option>";
-									}
-								}
-							 ?>
-				  		<!--
-							<option value="volvo" style="">A330-200</option>
-						-->
-					</select>
-				</th>
-				<th></th>
-				<th></th>
-				<th>
-					<input type="submit" value="Display Report">
-				</th>
-				<th>
-					<input type="submit" value="Export Excel">
-				</th>
-				<th>
-					<!-- <a href="index.php"><input type="button" value="Export PDF" /></a> -->
-					<input type="button" value="Export PDF" onclick="window.print()">
-				</th>
-			</tr>
-			<tr>
-				<th>
-					A/C Reg
-				</th>
-				<th>
-					<?php
-						echo '<input type="text" name="acreg" value="'.$_POST["acreg"].'">';
-					?>
-				</th>
-			</tr>
-			<tr>
-				<th>
-					Date from
-				</th>
-				<th>
-					<?php
-						echo '<input type="date" name="datefrom" id="id_datefrom" value="'.$_POST["datefrom"].'">';
-					?>
-				</th>
-				<th>
-					Date to
-				</th>
-				<th>
-					<?php
-						echo '<input type="date" name="dateto" id="id_dateto" value="'.$_POST["dateto"].'">';
-					?>
-				</th>
-			</tr>
-			<tr>
-				<th>
-					ATA
-				</th>
-				<th>
-					<?php
-						echo '<input type="text" name="ata" value="'.$_POST["ata"].'">';
-					?>
-				</th>
-			</tr>
-			<tr>
-				<th>
-					SUBATA
-				</th>
-				<th>
-					<?php
-						echo '<input type="text" name="subata" value="'.$_POST["subata"].'">';
-					?>
-				</th>
-			</tr>
-			<tr>
-			<tr>
-				<th>
-					Delay / Pirep
-				</th>
-				<th>
-					<?php
-						if($_POST["depir"]=="delay"){
-							?><input type="radio" name="depir" value="delay" id="radio_delay" onclick="check(this.value)" checked> Delay<?php
-						}
-						else{
-							?>
-							<input type="radio" name="depir" value="delay" id="radio_delay" onclick="check(this.value)"> Delay <?php
-						}
-						if($_POST["depir"]=="pirep"){?>
-							<input type="radio" name="depir" value="pirep" id="radio_pirep" onclick="check(this.value)" checked> Pirep<?php
-						}
-						else{?>
-							<input type="radio" name="depir" value="pirep" id="radio_pirep" onclick="check(this.value)"> Pirep <?php
-						}
-					?>
-				</th>
-			</tr>
-			</tr>
-				<th>
-					Keyword
-				</th>
-				<th>
-					<?php
-						echo '<input type="text" name="keyword" value="'.$_POST["keyword"].'">';
-					?>
-				</th>
-			</tr>
-			<tr>
-				<th>
-					DCP
-				</th>
-				<th>
+<form action="graph.php" method="post" class="form-horizontal style-form" style="margin-bottom: 50px" id="form_graph">
+
+	<div class="form-group">
+	  	<label class="col-sm-2 col-sm-2 control-label">A/C Type</label>
+	    <div class="col-sm-10">
+	      <select name="actype" class="form-control">
+	          <?php
+				$isSelect = "";
+				while($row = $res_actype->fetch_array(MYSQLI_NUM)){
+					if($row[0]==$_POST["actype"]){
+						$isSelect="selected";
+						echo "<option value=".$row[0]." ".$isSelect.">".$row[0]."</option>";
+					}
+					else{
+						echo "<option value=".$row[0].">".$row[0]."</option>";
+					}
+				}
+			 ?>
+	      </select>
+	    </div>
+	</div>
+
+	<div class="form-group">
+	    <label class="col-sm-2 col-sm-2 control-label">A/C Reg</label>
+	    <div class="col-sm-10">
+	    	<?php
+				echo '<input type="text" name="acreg" class="form-control" value="'.$_POST["acreg"].'">';
+			?>
+	    </div>
+	</div>
+			
+	<div class="form-group">
+	    <label class="col-sm-2 col-sm-2 control-label">Date from</label>
+	    <div class="col-sm-10">
+	    	<?php
+				echo '<input type="date" name="datefrom" class="form-control" id="id_datefrom" value="'.$_POST["datefrom"].'">';
+			?>
+	    </div>
+	</div>
+
+	<div class="form-group">
+	    <label class="col-sm-2 col-sm-2 control-label">Date to</label>
+	    <div class="col-sm-10">
+	    	<?php
+				echo '<input type="date" name="dateto" class="form-control" id="id_dateto" value="'.$_POST["dateto"].'">';
+			?>
+	    </div>
+	</div>
+
+	<div class="form-group">
+	    <label class="col-sm-2 col-sm-2 control-label">ATA</label>
+	    <div class="col-sm-10">
+	    	<?php
+				echo '<input type="text" name="ata" class="form-control" value="'.$_POST["ata"].'">';
+			?>
+	    </div>
+	</div>
+
+	<div class="form-group">
+	    <label class="col-sm-2 col-sm-2 control-label">SUBATA</label>
+	    <div class="col-sm-10">
+	    	<?php
+				echo '<input type="text" name="subata" class="form-control" value="'.$_POST["subata"].'">';
+			?>
+	    </div>
+	</div>
+
+	<div class="form-group">
+    <label class="col-sm-2 col-sm-2 control-label">Delay / Pirep</label>
+		<div class="col-sm-10">
+				<div class="radio">
+					<label>
+						<?php
+							if($_POST["depir"]=="delay"){
+								?><input type="radio" name="depir" value="delay" id="radio_delay" onclick="check(this.value)" checked> Delay<?php
+							}
+							else{
+								?>
+								<input type="radio" name="depir" value="delay" id="radio_delay" onclick="check(this.value)"> Delay <?php
+							}  
+						?>
+					</label>
+				</div>
+				<div class="radio">
+					<label>
+						<?php  
+							if($_POST["depir"]=="pirep"){?>
+								<input type="radio" name="depir" value="pirep" id="radio_pirep" onclick="check(this.value)" checked> Pirep<?php
+							}
+							else{?>
+								<input type="radio" name="depir" value="pirep" id="radio_pirep" onclick="check(this.value)"> Pirep <?php
+							}
+						?>
+					</label>
+				</div>
+		</div>
+  	</div>
+
+	<div class="form-group">
+	<label class="col-sm-2 col-sm-2 control-label">Keyword</label>
+		<div class="col-sm-10">
+			<?php
+				echo '<input type="text" name="keyword" class="form-control" value="'.$_POST["keyword"].'">';
+			?>
+		</div>
+	</div>
+
+	<div class="form-group">
+	    <label class="col-sm-2 col-sm-2 control-label">DCP</label>
+		<div class="col-sm-10">
 					<?php
 						if(empty($_POST['dcp'])){
 							$DCP = "";
@@ -145,32 +130,47 @@
 								$DCP[$i] = "";
 							}
 							if($i == 0 and $DCP[$i] != "d"  and $flag_d == 0){?>
-								<input type="checkbox" name="dcp[]" value="d"> D <?php
+								<label class="checkbox-inline">
+									<input type="checkbox" name="dcp[]" value="d"> D 
+								</label><?php
+
 							}
 							else if($i == 1 and $DCP[$i] != "c"  and $flag_c == 0){?>
-								<input type="checkbox" name="dcp[]" value="c"> C <?php
+								<label class="checkbox-inline">
+									<input type="checkbox" name="dcp[]" value="c"> C 
+								</label><?php
 							}
 							else if($i == 2 and $DCP[$i] != "x"  and $flag_x == 0){?>
-								<input type="checkbox" name="dcp[]" value="x"> X <?php
+								<label class="checkbox-inline">
+									<input type="checkbox" name="dcp[]" value="x"> X 
+								</label><?php
 							}
 							if($DCP[$i] == "d"){?>
-								<input type="checkbox" name="dcp[]" value="d" checked> D <?php
+								<label class="checkbox-inline">
+									<input type="checkbox" name="dcp[]" value="d" checked> D 
+								</label><?php
 								$flag_d = 1;
 							}else if($DCP[$i] == "c"){?>
-								<input type="checkbox" name="dcp[]" value="c" checked> C<?php
+								<label class="checkbox-inline">
+									<input type="checkbox" name="dcp[]" value="c" checked> C
+								</label><?php
 								$flag_c = 1;
 							}else if($DCP[$i] == "x"){?>
-								<input type="checkbox" name="dcp[]" value="x" checked> X <?php
+								<label class="checkbox-inline">
+									<input type="checkbox" name="dcp[]" value="x" checked> X 
+								</label><?php
 								$flag_x = 1;
 							}
 						}
 						//print_r($DCP);
 					?>
-				</th>
-			</tr>
-		</tbody>
-	</table>
+		</div>
+	</div>
+
+	<input type="submit" value="Display Report" class="btn btn-default">
+
 </form>
+
 <script type="text/javascript">
 	if(document.getElementById("radio_delay").checked)
 		check(document.getElementById("radio_delay").value);
---
+++ baru/input_mtbur.php
@@ -0,0 +1,51 @@
+<?php
+	$sql_actype = "SELECT DISTINCT ACtype FROM tbl_master_actype";
+	$res_actype = mysqli_query($link, $sql_actype);
+?>
+
+<h2> MTBUR </h2>
+
+<form action="mtbur.php" method="post" class="form-horizontal style-form" style="margin-bottom: 50px" id="form_mtbur">
+	<div class="form-group">
+		<label class="col-sm-2 col-sm-2 control-label">A/C Type</label>
+  		<div class="col-sm-10">
+  			<select name="actype" class="form-control">
+				<?php
+					$isSelect = "";
+					while($row = $res_actype->fetch_array(MYSQLI_NUM)){
+						if($row[0]==$_POST["actype"]){
+							$isSelect="selected";
+							echo "<option value=".$row[0]." ".$isSelect.">".$row[0]."</option>";
+						}
+						else{
+							echo "<option value=".$row[0].">".$row[0]."</option>";
+						}
+					}
+				 ?>
+		</select>
+  		</div>
+    </div>
+
+	<div class="form-group">
+  		<label class="col-sm-2 col-sm-2 control-label">Part Number</label>
+		<div class="col-sm-10">
+			<input type="text" name="partnumber" class="form-control">
+		</div>
+	</div>
+
+	<div class="form-group">
+  		<label class="col-sm-2 col-sm-2 control-label">Month from</label>
+		<div class="col-sm-10">
+			<input type="date" name="monthfrom" id="id_monthfrom" class="form-control">
+		</div>
+	</div>
+
+	<div class="form-group">
+  		<label class="col-sm-2 col-sm-2 control-label">Month to</label>
+		<div class="col-sm-10">
+			<input type="date" name="monthto" id="id_monthto" class="form-control">
+		</div>
+	</div>
+
+	<input type="submit" value="Display Report" class="btn btn-default">
+</form>
