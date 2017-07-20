f8533c4c7cc1df44c8f4e3deb6ba4391323cfb0f
MHR_7 <george_the@ymail.com>
2017-07-20 15:50:29 +0700

    tambah gambar buat front end

--- baru/footer.php
+++ baru/footer.php
@@ -2,7 +2,7 @@
 <!--footer start-->
 <footer class="site-footer" style="position:inherit; bottom:0; left:0; right:0;">
     <div class="text-center">
-        Copyright - TER1 - 2017
+        Copyright - Reliability Managemenet - 2017
         <a href="index.php" class="go-top">
             <i class="fa fa-angle-up"></i>
         </a>
